1fb1e1d686c814254f839c39908fda4c7c954a2e
Akake <georges.akake@kadea.local>
2023-08-03 12:58:39 +0000

    message

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //hello Georges
+const subjects = ["petit", "Je", "L'homme", "L'argent"];
+const verbs = ["sois", "peut", "doit", "donne"];
+const actions = ["poli", "tapper", "créer", "santé"];
+function generateRandomMessage() {
+  const subjects = subjects[Math.floor(Math.random() * subjects.length)];
+  const verbs = verbs[Math.floor(Math.random() * verbs.length)];
+  const actions = actions[Math.floor(Math.random() * actions.length)];
+  return message;
+}
+console.log(generateRandomMessage());
+
+
+
